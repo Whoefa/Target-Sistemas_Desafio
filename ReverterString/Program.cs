3d175840d0f226ae50613001ebcd77cb2ccf8712
Emanoel Faria <69714572+Whoefa@users.noreply.github.com>
2024-09-06 14:35:06 -0300

    Reverter Texto String

---
+++ ReverterString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Digite um texto para ser invertidor:");
+        string texto = Console.ReadLine();
+        
+        
+        ///Vou armazenar esse texto depois imprimir de maneira decrescente
+          
+            string resultado = "";
+            for (int i = texto.Length - 1; i >= 0; i--)
+            {
+                resultado += texto[i];
+            }
+            Console.WriteLine(resultado);
+        
+    }
+}
+
+   
+
