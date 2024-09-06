dd737325a73d724e6b2431d9f834a3e1aab75aaf
Emanoel Faria <69714572+Whoefa@users.noreply.github.com>
2024-09-06 12:34:52 -0300

    DesafioSomaK

---
+++ DesafioSomaK/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class DesafioSomaK
+{
+    public static void Main(string[] args)
+    {
+        int indice = 13, soma = 0, K = 0;
+
+        while (K < indice)
+        {
+            K = K + 1;
+            soma = soma + K;
+            Console.WriteLine($"Para K : {K} | Soma igual a {soma}");
+        }
+
+        Console.WriteLine("\n Soma final: " + soma);
+    }
+}
+
